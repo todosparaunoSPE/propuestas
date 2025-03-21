378586e02c89ea82f1f1856162b6872dd001c20a
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2025-03-21 09:16:54 -0600

    Update app3.py

--- app3.py
+++ app3.py
@@ -8,6 +8,24 @@ Created on Fri Mar 21 09:10:09 2025
 import streamlit as st
 import base64
 
+# Estilo de fondo
+page_bg_img = """
+<style>
+[data-testid="stAppViewContainer"]{
+background:
+radial-gradient(black 15%, transparent 16%) 0 0,
+radial-gradient(black 15%, transparent 16%) 8px 8px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 0 1px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 8px 9px;
+background-color:#282828;
+background-size:16px 16px;
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+
+
 # Función para mostrar el PDF en pantalla completa
 def show_pdf(pdf_file):
     with open(pdf_file, "rb") as f:
